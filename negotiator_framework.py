65cb50ad49fa624defdcc8d8a3471aa6f53937f7
luke <lukegessler@gmail.com>
2015-04-07 19:58:22 -0400

    Stuff

--- negotiator_framework.py
+++ negotiator_framework.py
@@ -2,6 +2,7 @@ from csv import DictReader
 from sys import argv, exit
 from itertools import islice
 from negotiator import Negotiator
+from naive_negotiator import NaiveNegotiator
 from random import seed, randint
 
 # read_scenario(parameterfile_name : String) --> (int, list(dict))
@@ -65,8 +66,8 @@ if __name__ == "__main__":
         exit(-42)
     score_a = score_b = 0
     # We will replace Negotiator here with <your id>_Negotiator, as specified in the Readme
-    negotiator_a = Negotiator()
-    negotiator_b = Negotiator()
+    negotiator_a = NaiveNegotiator()
+    negotiator_b = NaiveNegotiator()
     for scenario in argv[1:]:
         # Get the scenario parameters
         (num_iters, mapping) = read_scenario(scenario)
