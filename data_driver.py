e9af085da134fe2d36cd520f2d5637838c3afc64
mqn2at <minhqn127@gmail.com>
2015-04-12 19:23:04 -0400

    made more test negotiators and made a driver for testing our negotiator for report

---
+++ data_driver.py
@@ -0,0 +1,117 @@
+from negotiator_framework import negotiate,read_scenario
+from itertools import permutations, combinations
+import os
+
+from naive_negotiator import NaiveNegotiator
+#from negotiator import Negotiator
+from test_negotiators import Selfish_Negotiator, Mostly_Selfish_Negotiator, LenientNegotiator, CavingNegotiator, RandomWithThresholdNegotiator
+from accommodating_negotiator import AccommodatingNegotiator
+
+negotiators = [
+    NaiveNegotiator(),
+    NaiveNegotiator(),
+    Selfish_Negotiator(),
+	Selfish_Negotiator(),
+    Mostly_Selfish_Negotiator(),
+	Mostly_Selfish_Negotiator(),
+    LenientNegotiator(),
+    LenientNegotiator(),
+    CavingNegotiator(),
+    CavingNegotiator(),
+    RandomWithThresholdNegotiator(),
+    RandomWithThresholdNegotiator(),
+    AccommodatingNegotiator(),
+    AccommodatingNegotiator()
+]
+wltf = [[0,0,0,0] for n in negotiators]
+
+performance = {x : 0 for x in negotiators}
+#csvs = [os.path.join('test_cases',x) for x in os.listdir('test_cases')]
+#csvs = [os.path.join('gen_cases',x) for x in os.listdir('gen_cases')]
+csvs = [os.path.join('5-items',x) for x in os.listdir('5-items')]
+#csvs = [os.path.join('7-items',x) for x in os.listdir('7-items')]
+#csvs = [os.path.join('10-items',x) for x in os.listdir('10-items')]
+
+pair_results = { x : {y : {"W":0,"L":0,"D":0,"F":0} for y in range(len(negotiators)) if y != x} for x in range(len(negotiators))}
+#print "PAIRS"
+#print pair_results
+
+def fight_all_csvs(negotiator_a, negotiator_b):
+    print("Pitting %s and %s..." % (negotiator_a.__class__.__name__,negotiator_b.__class__.__name__))
+    score_a = score_b = 0
+    for scenario in csvs:
+        # Get the scenario parameters
+        (num_iters, mapping) = read_scenario(scenario)
+        # Separate the mapping out for each negotiator, and sort the items from it into a list
+        # based upon the preferences of each negotiator
+        a_mapping = {item["item_name"] : int(item["negotiator_a"]) for item in mapping}
+        a_order = sorted(a_mapping, key=a_mapping.get, reverse=True)
+        b_mapping = {item["item_name"] : int(item["negotiator_b"]) for item in mapping}
+        b_order = sorted(b_mapping, key=b_mapping.get, reverse=True)
+        # Give each negotiator their preferred item ordering
+        negotiator_a.initialize(a_order, num_iters)
+        negotiator_b.initialize(b_order, num_iters)
+        for j in range(5):
+            # Get the result of the negotiation
+            (result, order, count) = negotiate(num_iters, negotiator_a, negotiator_b)
+            # Assign points to each negotiator. Note that if the negotiation failed, each negotiatior receives a negative penalty
+            # However, it is also possible in a "successful" negotiation for a given negotiator to receive negative points
+            (points_a, points_b) = (negotiator_a.utility(), negotiator_b.utility()) if result else (-len(a_order), -len(b_order))
+            results = (result, points_a, points_b, count)
+            score_a += points_a
+            score_b += points_b
+
+            #Store results for stats
+            if not result:
+                pair_results[negotiators.index(negotiator_a)][negotiators.index(negotiator_b)]["F"] += 1
+                pair_results[negotiators.index(negotiator_b)][negotiators.index(negotiator_a)]["F"] += 1
+            else:
+                if points_a > points_b:
+                    pair_results[negotiators.index(negotiator_a)][negotiators.index(negotiator_b)]["W"] += 1
+                    pair_results[negotiators.index(negotiator_b)][negotiators.index(negotiator_a)]["L"] += 1
+                elif points_a < points_b:
+                    pair_results[negotiators.index(negotiator_a)][negotiators.index(negotiator_b)]["L"] += 1
+                    pair_results[negotiators.index(negotiator_b)][negotiators.index(negotiator_a)]["W"] += 1
+                else:
+                    pair_results[negotiators.index(negotiator_a)][negotiators.index(negotiator_b)]["D"] += 1
+                    pair_results[negotiators.index(negotiator_b)][negotiators.index(negotiator_a)]["D"] += 1
+
+            # Update each negotiator with the final result, points assigned, and number of iterations taken to reach an agreement
+            negotiator_a.receive_results(results)
+            negotiator_b.receive_results(results)
+            #print("{} negotiation:\n\t{}: {}\n\t{}: {}".format("Successful" if result else "Failed", negotiator_a.__class__.__name__, points_a, negotiator_b.__class__.__name__, points_b))
+    #print("Final result:\n\t{}: {}\n\t{}: {}".format(negotiator_a.__class__.__name__,score_a, negotiator_b.__class__.__name__,score_b))
+    return score_a,score_b
+
+for i in range(2):
+    for a,b in permutations(negotiators, 2):
+        sa, sb = fight_all_csvs(a,b)
+        performance[a] += sa
+        performance[b] += sb
+
+#Print scores of every negotiator
+print("SCORES:")
+for neg,score in performance.items():
+    print(negotiators.index(neg),neg.__class__.__name__,score)
+print()
+
+#Print w/l ratios
+print("WIN/LOSS RATIOS")
+for neg_a in pair_results:
+    print(neg_a,negotiators[neg_a].__class__.__name__)
+    print("\tWins:",sum([pair_results[neg_a][neg_b]["W"] for neg_b in pair_results[neg_a]]))
+    print("\tLosses:",sum([pair_results[neg_a][neg_b]["L"] for neg_b in pair_results[neg_a]]))
+    print("\tDraws:",sum([pair_results[neg_a][neg_b]["D"] for neg_b in pair_results[neg_a]]))
+    print("\tFailed Negotations:",sum([pair_results[neg_a][neg_b]["F"] for neg_b in pair_results[neg_a]]))
+print() 
+
+#Print pairing data
+print("VS DATA:")
+for neg_a in pair_results:
+    print(neg_a,negotiators[neg_a].__class__.__name__)
+    for neg_b in pair_results[neg_a]:
+        print("\t",neg_b,negotiators[neg_b].__class__.__name__)
+        print("\t\tWins:",pair_results[neg_a][neg_b]["W"])
+        print("\t\tLosses:",pair_results[neg_a][neg_b]["L"])
+        print("\t\tDraws:",pair_results[neg_a][neg_b]["D"])
+        print("\t\tFailed Negotations:",pair_results[neg_a][neg_b]["F"])
--- test_negotiators.py
+++ test_negotiators.py
@@ -39,3 +39,252 @@ class Mostly_Selfish_Negotiator(BaseNegotiator):
         util = self.utility()
         self.offer = temp
         return util
+
+class LenientNegotiator(BaseNegotiator):
+    def __init__(self):
+        self.preferences = []
+        self.offer = []
+        self.iter_limit = 0
+
+        self.is_first = False
+        self.iter = 0
+        self.thresh = 0.3
+        self.max_util = 0.0
+        self.min_util = 0.0
+        self.offer_index = 0
+
+    def find_util(self, order):
+        tmp = self.offer[:]
+        self.offer = order[:]
+        i = self.utility()
+        self.offer = tmp
+        return i
+
+    def find_possibilities(self):
+        #return sorted([(a,self.find_util(a)) for a in prm(self.preferences)],key=lambda x:x[1])
+        L = [self.preferences[:]]
+        outlist = []
+        while True:
+            tmp = L.pop(0)
+            tmputil = self.find_util(tmp)
+            if tmputil < self.thresh * self.find_util(self.preferences):
+                break
+            outlist.append((tmp,tmputil))
+
+            for i in range(len(tmp)):
+                for j in range(len(tmp)):
+                    if i != j:
+                        swapt = tmp[:]
+                        swapt[i], swapt[j] = swapt[j], swapt[i]
+                        if swapt not in L:
+                            L.append(swapt)
+
+        return sorted(outlist,key=lambda x: x[1])
+
+    def initialize(self, preferences, iter_limit):
+        self.preferences = preferences
+        self.iter_limit = iter_limit
+        self.iter = 0
+        self.sp = self.find_possibilities() 
+        self.sp = self.sp[::-1]
+        self.max_util = self.sp[0][1]
+        self.min_util = self.thresh * self.max_util
+
+    def make_offer(self, offer):
+    #check if first in first iteration
+        # assume no agent ever gives an offer of None
+        if offer == None:
+            self.is_first = True
+        else:
+            self.iter += 1
+
+        #if last offer
+        if self.iter == self.iter_limit:
+            self.iter = 0
+            if not self.is_first:
+                self.offer = self.sp[0][0]
+                return self.offer
+            
+            #Final accept
+            elif self.find_util(offer) >= self.min_util:
+                self.offer = offer[:]
+                return offer
+            #Final decline
+            else:
+                self.offer = self.sp[0][0]
+                return self.offer
+
+        # sorted possibilities
+        if offer is None or (self.find_util(offer) < self.sp[self.offer_index][1]):
+            self.offer = self.sp[self.offer_index][0]
+            self.offer_index = (self.offer_index + 1) % len(self.sp)
+            #If offer starts being bad, reset offers
+            if self.sp[self.offer_index][1] < self.min_util:
+                self.offer_index = 0
+            return self.offer
+
+        self.offer = offer
+        return self.offer
+
+    def receive_utility(self, utility):
+        self.opponents_utility = utility #literally never used
+
+    # receive_results(self : BaseNegotiator, results : (Boolean, Float, Float, Int))
+        # Store the results of the last series of negotiation (points won, success, etc.)
+    def receive_results(self, results):
+        #If negotiation succeeded
+        if results[0]:
+            temp = 0.0
+            if self.is_first:
+                temp = results[1]
+            else:
+                temp = results[2]
+            if temp > self.min_util:
+                self.min_util = temp
+        #If negotiation failed
+        else:
+            #Do nothing yet
+            return
+
+class CavingNegotiator(BaseNegotiator):
+    def __init__(self):
+        self.preferences = []
+        self.offer = []
+        self.iter_limit = 0
+
+        self.is_first = False
+        self.iter = 0
+        self.thresh = 0.6
+        self.max_util = 0.0
+        self.min_util = 0.0
+        self.offer_index = 0
+
+    def find_util(self, order):
+        tmp = self.offer[:]
+        self.offer = order[:]
+        i = self.utility()
+        self.offer = tmp
+        return i
+
+    def find_possibilities(self):
+        #return sorted([(a,self.find_util(a)) for a in prm(self.preferences)],key=lambda x:x[1])
+        L = [self.preferences[:]]
+        outlist = []
+        while True:
+            tmp = L.pop(0)
+            tmputil = self.find_util(tmp)
+            if tmputil < self.thresh * self.find_util(self.preferences):
+                break
+            outlist.append((tmp,tmputil))
+
+            for i in range(len(tmp)):
+                for j in range(len(tmp)):
+                    if i != j:
+                        swapt = tmp[:]
+                        swapt[i], swapt[j] = swapt[j], swapt[i]
+                        if swapt not in L:
+                            L.append(swapt)
+
+        return sorted(outlist,key=lambda x: x[1])
+
+    def initialize(self, preferences, iter_limit):
+        self.preferences = preferences
+        self.iter_limit = iter_limit
+        self.iter = 0
+        self.sp = self.find_possibilities() 
+        self.sp = self.sp[::-1]
+        self.max_util = self.sp[0][1]
+        self.min_util = self.thresh * self.max_util
+
+    def make_offer(self, offer):
+    #check if first in first iteration
+        # assume no agent ever gives an offer of None
+        if offer == None:
+            self.is_first = True
+        else:
+            self.iter += 1
+
+        #if last offer
+        if self.iter == self.iter_limit:
+            self.iter = 0
+            if not self.is_first:
+                self.offer = self.sp[0][0]
+                return self.offer
+            
+            #Final accept
+            else:
+                self.offer = offer[:]
+                return offer
+
+        # sorted possibilities
+        if offer is None or (self.find_util(offer) < self.sp[self.offer_index][1]):
+            self.offer = self.sp[self.offer_index][0]
+            self.offer_index = (self.offer_index + 1) % len(self.sp)
+            #If offer starts being bad, reset offers
+            if self.sp[self.offer_index][1] < self.min_util:
+                self.offer_index = 0
+            return self.offer
+
+        self.offer = offer
+        return self.offer
+
+    def receive_utility(self, utility):
+        self.opponents_utility = utility #literally never used
+
+    # receive_results(self : BaseNegotiator, results : (Boolean, Float, Float, Int))
+        # Store the results of the last series of negotiation (points won, success, etc.)
+    def receive_results(self, results):
+        #If negotiation succeeded
+        if results[0]:
+            temp = 0.0
+            if self.is_first:
+                temp = results[1]
+            else:
+                temp = results[2]
+            if temp > self.min_util:
+                self.min_util = temp
+        #If negotiation failed
+        else:
+            #Do nothing yet
+            return
+
+
+class RandomNegotiator(BaseNegotiator):
+    # Override the make_offer method from BaseNegotiator to accept a given offer 5%
+    # of the time, and return a random permutation the rest of the time.   
+    def make_offer(self, offer):
+        if random() < 0.2 and offer:
+            # Very important - we save the offer we're going to return as self.offer
+            self.offer = offer[:]
+            return offer
+        else:
+            ordering = self.preferences
+            shuffle(ordering)
+            self.offer = ordering[:]
+            return self.offer
+
+
+
+class RandomWithThresholdNegotiator(BaseNegotiator):
+    threshold = 0.5
+    def calc_utility(self,offer):
+        temp = self.offer
+        self.offer = offer
+        util = self.utility()
+        self.offer = temp
+        return util
+
+    # Override the make_offer method from BaseNegotiator to accept a given offer 5%
+    # of the time, and return a random permutation the rest of the time.   
+    def make_offer(self, offer):
+        if offer != None and float(self.calc_utility(offer)) >= self.threshold * float(self.calc_utility(self.preferences)):
+            # Very important - we save the offer we're going to return as self.offer
+            self.offer = offer[:]
+            return offer
+        else:
+            ordering = self.preferences
+            shuffle(ordering)
+            while float(self.calc_utility(ordering)) < self.threshold * float(self.calc_utility(self.preferences)):
+                shuffle(ordering)
+            self.offer = ordering[:]
+            return self.offer
